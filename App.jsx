9d62fb8cd32bc87e10f45e2487753948866fda3f
Faraz Mehmood <farazmehmood003@gmail.com>
2026-09-10 10:11:08 +0500

    Add Paradise Nursery App component

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <main className="landing-page">
+      <section className="hero">
+        <div className="hero-content">
+          <h1>Paradise Nursery</h1>
+          <p>Bring the beauty of nature into your home with our beautiful collection of plants.</p>
+          <button className="get-started">Get Started</button>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default App;
